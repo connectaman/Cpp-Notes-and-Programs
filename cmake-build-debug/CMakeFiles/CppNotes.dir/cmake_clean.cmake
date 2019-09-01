28479a9da39b79790dbc79c0e0f7276385da42c0
connectamanulla <connectamanulla@gmail.com>
2019-09-01 11:42:24 +0530

    initialized git

---
+++ cmake-build-debug/CMakeFiles/CppNotes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppNotes.dir/main.cpp.o"
+  "CppNotes.pdb"
+  "CppNotes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppNotes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
